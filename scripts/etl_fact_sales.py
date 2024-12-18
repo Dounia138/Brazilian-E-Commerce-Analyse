a229c63cc09a4183f8f228634709fd220411cae9
Taha2k3 <taha-yassine.nyouri@supdevinci-edu.fr>
2024-12-18 14:33:56 +0100

    Update etl_fact_sales.py

--- scripts/etl_fact_sales.py
+++ scripts/etl_fact_sales.py
@@ -16,18 +16,20 @@ DATA_LAKE_PATH = "../DataLake"
 
 def process_fact_sales():
     print("Traitement de la table fact_sales...")
-    orders = pd.read_csv(os.path.join(DATA_LAKE_PATH, "orders/olist_orders_dataset.csv", columns = ["order_id","customer_id"]))
-    items = pd.read_csv(os.path.join(DATA_LAKE_PATH, "items/olist_order_items_dataset.csv",  columns = ["order_id","product_id", "seller_id", "price", "freight_value", "order_item_id"]))
-    payments = pd.read_csv(os.path.join(DATA_LAKE_PATH, "payments/olist_order_payments_dataset.csv",  columns = ["order_id","payment_value"]))
+    df_order = pd.read_csv(os.path.join(DATA_LAKE_PATH, "orders/olist_orders_dataset.csv", columns = ["order_id","customer_id"]))
+    df_order_item = pd.read_csv(os.path.join(DATA_LAKE_PATH, "items/olist_order_items_dataset.csv",  columns = ["order_id","product_id", "seller_id", "price", "freight_value", "order_item_id"]))
+    df_payment = pd.read_csv(os.path.join(DATA_LAKE_PATH, "payments/olist_order_payments_dataset.csv",  columns = ["order_id","payment_value"]))
 
-    sales = orders.merge(items, on='order_id', how='full') \
-                .merge(payments, on = 'order_id', how='full')
-                  
-    sales_clean = sales.dropna().drop_duplicates()
-    
+    df_sales = pd.merge(df_order_item,df_order, on='order_id', how='outer')
+    dff_sales = pd.merge(df_sales,df_payment, on = 'order_id', how = 'outer')
+    dff_sales['date_id'] = pd.to_datetime(dff_sales['order_purchase_timestamp']).dt.date
+    dff_sales = dff_sales[['order_id','order_item_id','product_id','seller_id','date_id','freight_value','price','payment_value','customer_id']]
+    df_sales_rename = dff_sales.rename(columns = {'order_item_id':'order_item','price':'item_price'})
+
+
     # Insertion dans PostgreSQL
     engine = create_engine(DATABASE_URL)
-    sales_clean.to_sql('fact_sales', engine, if_exists='append', index=False)
+    df_sales_rename.to_sql('fact_sales', engine, if_exists='append', index=False)
     print("fact_sales traitée et chargée avec succès.")
 
 if __name__ == "__main__":
